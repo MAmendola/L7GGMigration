bdabae433c2f83dcef84d74ef1f27c5d5f1af3af
LAPTOP-BGQA73OC\ahmed <ahmedovadurdona@yahoo.com>
2021-02-28 22:39:31 -0500

    commit sns file

--- sns.tf
+++ sns.tf
@@ -1,27 +1,27 @@
-resource "aws_sns_topic" "team2" {
-  name = "team2"
-  delivery_policy = <<EOF
-{
-  "http": {
-    "defaultHealthyRetryPolicy": {
-      "minDelayTarget": 20,
-      "maxDelayTarget": 20,
-      "numRetries": 3,
-      "numMaxDelayRetries": 0,
-      "numNoDelayRetries": 0,
-      "numMinDelayRetries": 0,
-      "backoffFunction": "linear"
-    },
-    "disableSubscriptionOverrides": false,
-    "defaultThrottlePolicy": {
-      "maxReceivesPerSecond": 1
-    }
-  }
-}
-EOF
-}
-resource "aws_sns_topic_subscription" "team2" {
-  topic_arn = "arn:aws:sns:us-west-2:086790382789:team2"
-  protocol  = "sms"
-  endpoint  = "phone number"
-}
+# resource "aws_sns_topic" "team2" {
+#   name = "team2"
+#   delivery_policy = <<EOF
+# {
+#   "http": {
+#     "defaultHealthyRetryPolicy": {
+#       "minDelayTarget": 20,
+#       "maxDelayTarget": 20,
+#       "numRetries": 3,
+#       "numMaxDelayRetries": 0,
+#       "numNoDelayRetries": 0,
+#       "numMinDelayRetries": 0,
+#       "backoffFunction": "linear"
+#     },
+#     "disableSubscriptionOverrides": false,
+#     "defaultThrottlePolicy": {
+#       "maxReceivesPerSecond": 1
+#     }
+#   }
+# }
+# EOF
+# }
+# resource "aws_sns_topic_subscription" "team2" {
+#   topic_arn = "arn:aws:sns:us-west-2:086790382789:team2"
+#   protocol  = "sms"
+#   endpoint  = "phone number"# needs phone number
+# }

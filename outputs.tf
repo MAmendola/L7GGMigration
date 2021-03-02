7aa88fafaecf4af702e095a35bf9f471656fffef
MAmendola <mamendola@ziyotek.net>
2021-03-02 16:23:43 -0500

    added cloudfront_id

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "elb_dns_name" {
   value       = aws_lb.web_lb.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "cloudfront_id" {
+    value = aws_cloudfront_distribution.s3_distribution.id
+}

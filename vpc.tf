0ef33611271e670f91084b99af650ae02397fde3
Alex Malai <alexmalai@Alexs-Air.fios-router.home>
2021-03-02 19:50:47 -0500

    Merge to master and vpc fix2

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-# resource "aws_vpc" "team2vpc" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
-
-#   tags = {
-#     Name = "main"
-#   }
-# }
-=======
+# 
 resource "aws_vpc" "team2vpc" {
   cidr_block = "10.0.0.0/16"
 

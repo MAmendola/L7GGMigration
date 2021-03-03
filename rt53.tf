5b9d5bd05e41e76baf5e66433307afc86ef33e99
Miguel Alfaro <miguel0926@gmail.com>
2021-03-03 23:27:44 +0000

    Update rt53.tf

--- rt53.tf
+++ rt53.tf
@@ -4,15 +4,15 @@ resource "aws_route53_zone" "gogreen" {
 }
 
 //Creates record in hosted zone RT53
-resource "aws_route53_record" "www1" {
+resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.gogreen.zone_id
   name    = "www.globalebsolutions.com"
   type    = "A"
   alias {
-    name                   = aws_lb.web_lb.dns_name //resolves dns name to webtier lb dns name
-    zone_id                = aws_lb.web_lb.zone_id
+    name    = "dualstack.${aws_lb.web_lb.dns_name}"  //resolves dns name to webtier lb dns name
+    zone_id = aws_lb.web_lb.zone_id   
     evaluate_target_health = true
-
+    
   }
 }
 

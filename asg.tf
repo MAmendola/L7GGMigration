4c932949ce13a4847198d49fe8c072ebb6957ca3
MAmendola <mamendola@ziyotek.net>
2021-03-03 19:18:22 -0500

    fixed security group

--- asg.tf
+++ asg.tf
@@ -90,6 +90,13 @@ resource "aws_security_group" "elb-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+      ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.db_sec_group.id] #------the surce is lb in webtier to test only
+  }
+
 
   egress {
     from_port   = 0
--- route 53.tf
+++ route 53.tf
@@ -1,17 +1,18 @@
 
 
-resource "aws_route53_zone" "l7gogreen" {
-  name = "l7gogreen.com"
+# resource "aws_route53_zone" "l7gogreen" {
+#   name = "l7gogreen.com"
 
-}
+# }
 
-resource "aws_route53_record" "www1" {
-  zone_id = aws_route53_zone.l7gogreen.zone_id
-  name    = "www.l7gogreen.com"
-  type    = "A"
-  # records = [aws_route53_zone.l7gogreen.com]
-  # alias {
-  #   name    = aws_cloudfront.www.l7gogreen.com
-  #   zone_id = aws_cloudfront.zone.l7gogreen.zone_id
-  # }
-}
+# resource "aws_route53_record" "www1" {
+#   zone_id = aws_route53_zone.l7gogreen.zone_id
+#   name    = "www.l7gogreen.com"
+#   ttl = "300"
+#   type    = "A"
+#   records = [aws_route53_zone.l7gogreen.name]
+#   # alias {
+#   #   name    = aws_cloudfront.www.l7gogreen.com
+#   #   zone_id = aws_cloudfront.zone.l7gogreen.zone_id
+#   # }
+# }
